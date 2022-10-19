2eff59ca49f87d4adda09e2d9a69be35a9c02871
Ahmet <35678867+ahmetmutlugun@users.noreply.github.com>
2022-10-18 20:33:41 -0700

    Enabled logging and small optimizations

--- sandbox.py
+++ sandbox.py
@@ -3,11 +3,10 @@ import aiohttp
 import requests
 import json
 import firebase
-
+import logging
 from aiohttp import ContentTypeError
 
-
-
+logging.basicConfig(level=logging.DEBUG)
 f = open('config.json')
 config = json.load(f)
 f.close()
@@ -15,7 +14,7 @@ f.close()
 proxies = config['proxies']
 username = config['username']
 password = config['password']
-
+proxy_auth = aiohttp.BasicAuth(username, password)
 results = []
 
 
@@ -23,18 +22,20 @@ async def get_price(session, url, name):
     proxy = proxies.pop(0)
     proxies.append(proxy)
 
-    proxy_auth = aiohttp.BasicAuth(username, password)
-    async with session.get(url, proxy=proxy, proxy_auth=proxy_auth) as resp:
-        data = {"failed": True}
+    try:
+        async with session.get(url, proxy=proxy, proxy_auth=proxy_auth) as resp:
+            data = {"failed": True}
 
-        try:
-            data = await resp.json()
-            if data is None:
-                data = {"failed": True}
-        except ContentTypeError:
-            print(await resp.text())
+            try:
+                data = await resp.json()
+                if data is None:
+                    data = {"failed": True}
+            except ContentTypeError:
+                print(await resp.text())
 
-        return name, data
+            return name, data
+    except TimeoutError:
+        return name, {"failed": True}
 
 
 async def start(items):
@@ -46,13 +47,13 @@ async def start(items):
 
         original_item = await asyncio.gather(*tasks)
         firebase.set_item(original_item)
-        await asyncio.sleep(10)
+        await asyncio.sleep(5)
 
 
 if __name__ == "__main__":
     count = 0
-    all_items = requests.get("https://api.steamapis.com/image/items/730")
-    item_queue = list(all_items.json().keys())[-1::]
+    item_queue = list(requests.get("https://api.steamapis.com/image/items/730").json().keys())
+    logging.info("Item Queue Initialized")
     length = len(item_queue)
 
     while True:
@@ -63,7 +64,7 @@ if __name__ == "__main__":
             item_queue.append(item)
         count += 10
         if count > length:
-            items = requests.get("https://api.steamapis.com/image/items/730")
-            item_queue = list(items.json().keys())
+            item_queue = list(requests.get("https://api.steamapis.com/image/items/730").json().keys())
             length = len(item_queue)
-        print(f"{count}/{length}")
+            count = 0
+        logging.info(f"{count}/{length}")
